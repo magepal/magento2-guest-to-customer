ef300e29260cf658549b4ebbd7b61e0d0c321668
Marc <xerokill@gmail.com>
2019-05-23 11:24:01 +0800

    allow configuration of customer details which will be auto-merged into guest order during conversion

--- Helper/Data.php
+++ Helper/Data.php
@@ -14,10 +14,15 @@ use Magento\Framework\ObjectManagerInterface;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Store\Model\ScopeInterface;
 
-class Data extends AbstractHelper
-{
+class Data extends AbstractHelper {
     const XML_PATH_ACTIVE = 'guesttocustomer/general/active';
     const XML_CUSTOMER_DASHBOARD = 'guesttocustomer/general/customer_dashboard';
+
+    const XML_MERGE_CUSTOMER_GROUP = 'guesttocustomer/merge/group';
+    const XML_MERGE_CUSTOMER_NAME = 'guesttocustomer/merge/name';
+    const XML_MERGE_CUSTOMER_DOB = 'guesttocustomer/merge/dob';
+    const XML_MERGE_CUSTOMER_GENDER = 'guesttocustomer/merge/gender';
+    const XML_MERGE_CUSTOMER_TAXVAT = 'guesttocustomer/merge/taxvat';
 
     /**
      * @param Context $context
@@ -34,12 +39,8 @@ class Data extends AbstractHelper
      *
      * @return bool
      */
-    public function isEnabled()
-    {
-        return $this->scopeConfig->isSetFlag(
-            self::XML_PATH_ACTIVE,
-            ScopeInterface::SCOPE_STORE
-        );
+    public function isEnabled() {
+        return $this->_isEnabled(self::XML_PATH_ACTIVE);
     }
 
     /**
@@ -47,12 +48,8 @@ class Data extends AbstractHelper
      *
      * @return bool
      */
-    public function isEnabledCustomerDashboard()
-    {
-        return $this->isEnabled() && $this->scopeConfig->getValue(
-            self::XML_CUSTOMER_DASHBOARD,
-            ScopeInterface::SCOPE_STORE
-        );
+    public function isEnabledCustomerDashboard() {
+        return $this->isEnabled() && $this->_isEnabled(self::XML_CUSTOMER_DASHBOARD);
     }
 
     /**
@@ -60,8 +57,7 @@ class Data extends AbstractHelper
      * @param $orderId int
      * @return $this
      */
-    public function dispatchCustomerOrderLinkEvent($customerId, $orderId)
-    {
+    public function dispatchCustomerOrderLinkEvent($customerId, $orderId) {
         $this->_eventManager->dispatch('magepal_guest_to_customer_save', [
             'customer_id' => $customerId,
             'order_id' => $orderId, //incrementId
@@ -74,19 +70,47 @@ class Data extends AbstractHelper
     /**
      * @param $order OrderInterface
      * @param $customer CustomerInterface
-     * @return $order
      */
     public function setCustomerData(OrderInterface $order, CustomerInterface $customer) {
         $order->setCustomerIsGuest(0);
         $order->setCustomerId($customer->getId());
-        $order->setCustomerGroupId($customer->getGroupId());
-        $order->setCustomerDob($customer->getDob());
-        $order->setCustomerFirstname($customer->getFirstname());
-        $order->setCustomerLastname($customer->getLastname());
-        $order->setCustomerMiddlename($customer->getMiddlename());
-        $order->setCustomerPrefix($customer->getPrefix());
-        $order->setCustomerSuffix($customer->getSuffix());
-        $order->setCustomerTaxvat($customer->getTaxvat());
-        $order->setCustomerGender($customer->getGender());
+
+        if ($this->_isEnabled(self::XML_MERGE_CUSTOMER_GROUP)) {
+            $order->setCustomerGroupId($customer->getGroupId());
+        }
+
+        if ($this->_isEnabled(self::XML_MERGE_CUSTOMER_NAME)) {
+            $order->setCustomerPrefix($customer->getPrefix());
+
+            $order->setCustomerFirstname($customer->getFirstname());
+            $order->setCustomerLastname($customer->getLastname());
+            $order->setCustomerMiddlename($customer->getMiddlename());
+
+            $order->setCustomerSuffix($customer->getSuffix());
+        }
+
+        if ($this->_isEnabled(self::XML_MERGE_CUSTOMER_DOB)) {
+            $order->setCustomerDob($customer->getDob());
+        }
+
+        if ($this->_isEnabled(self::XML_MERGE_CUSTOMER_GENDER)) {
+            $order->setCustomerGender($customer->getGender());
+        }
+
+        if ($this->_isEnabled(self::XML_MERGE_CUSTOMER_TAXVAT)) {
+            $order->setCustomerTaxvat($customer->getTaxvat());
+        }
+    }
+
+    /**
+     * @param $xmlPath string
+     *
+     * @return boolean
+     */
+    protected function _isEnabled($xmlPath) {
+        return $this->scopeConfig->isSetFlag(
+            $xmlPath,
+            ScopeInterface::SCOPE_STORE
+        );
     }
 }
